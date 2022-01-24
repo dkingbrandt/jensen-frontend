3bdd8fda52e43373e01786712564feab97fb3dd2
dkingbrandt <dkingbrandt@hotmail.com>
2022-01-24 21:09:49 +0100

    heroku

--- app.js
+++ app.js
@@ -1,18 +1,23 @@
-document.addEventListener("submit" , (event)=>{
-  console.log("Validating")
+
+
+document.addEventListener("submit", (event) => {
+  console.log("Validating");
   let user = document.getElementById("user").value;
   let password = document.getElementById("password").value;
 
+  
   let request = new XMLHttpRequest();
-  request.open("POST", "http://localhost:5500/authorize");
+  let url =  "https://dan-backend.herokuapp.com/"
+  request.open("GET", url);
+  
   //request.send(new FormData(formElement));
-  console.log("Validated")
+  console.log("Validated");
 
   request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
   request.send(`user=${user}&password=${password}`);
 
-  event.preventDefault()
-})
+  event.preventDefault();
+});
 //end of coocie test//
 // Add cookie popup
 // set cookie according to you
@@ -23,26 +28,26 @@ var cookieExpireDays = 30;
 // when users click accept button
 let acceptCookie = document.getElementById("acceptCookie");
 acceptCookie.onclick = function () {
-createCookie(cookieName, cookieValue, cookieExpireDays);
-//uppdaterar
-window.location.reload()
+  createCookie(cookieName, cookieValue, cookieExpireDays);
+  //uppdaterar
+  window.location.reload();
 };
 
 // function to set cookie in web browser
 let createCookie = function (cookieName, cookieValue, cookieExpireDays) {
-let currentDate = new Date();
-currentDate.setTime(
-  currentDate.getTime() + cookieExpireDays * 24 * 60 * 60 * 1000
-);
-let expires = "expires=" + currentDate.toGMTString();
-document.cookie = cookieName + "=" + cookieValue + ";" + expires + ";path=/";
-if (document.cookie) {
-  document.getElementById("cookiePopup").style.display = "none";
-} else {
-  alert(
-    "Unable to set cookie. Please allow all cookies site from cookie setting of your browser"
+  let currentDate = new Date();
+  currentDate.setTime(
+    currentDate.getTime() + cookieExpireDays * 24 * 60 * 60 * 1000
   );
-}
+  let expires = "expires=" + currentDate.toGMTString();
+  document.cookie = cookieName + "=" + cookieValue + ";" + expires + ";path=/";
+  if (document.cookie) {
+    document.getElementById("cookiePopup").style.display = "none";
+  } else {
+    alert(
+      "Unable to set cookie. Please allow all cookies site from cookie setting of your browser"
+    );
+  }
 };
 // get cookie from the web browser
 let getCookie = function (cookieName) {
@@ -74,38 +79,67 @@ let checkCookie = function () {
 
     // function recaptcha_callback() {
     //   let registerBtn = document.querySelector("#register-btn");
-    //   registerBtn.removeAttribute("disabled"); 
+    //   registerBtn.removeAttribute("disabled");
     //   registerBtn.style.cursor = "pointer";
-
-
-
-    }
-
-    // recaptcha_callback(); 
-
   }
+
+  // recaptcha_callback();
+};
 
 checkCookie();
 
-
-
-
-  function recaptcha_callback() {
-    let registerBtn = document.querySelector("#register-btn");
-    registerBtn.removeAttribute("disabled"); 
-    registerBtn.style.cursor = "pointer"; 
-
-
-    }
-    function hiddenfunction() {
-      var password = document.getElementById("password")
-      var user = document.getElementById("user")
-      let x = document.getElementById("deleteContainer");
-      if (x.style.display === "none") {
-        x.style.display = "block";
-      } else if ( password.value === "password" && user.value === "user") {
-        x.style.display = "none";
-      } 
-    recaptcha_callback()
-
+function recaptcha_callback() {
+  let registerBtn = document.querySelector("#login-btn");
+  registerBtn.removeAttribute("disabled");
+  registerBtn.style.cursor = "pointer";
 }
+function hiddenfunction() {
+  var password = document.getElementById("password");
+  var user = document.getElementById("user");
+  let x = document.getElementById("deleteContainer");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else if (
+    (password.value === "password" && user.value === "user") ||
+    (password.value === "Pannika" && user.value === "Annika") ||
+    (password.value === "Pan" && user.value === "Dan") ||
+    (password.value === "Bläbläblä" && user.value === "Rebecca")
+  ) {
+    x.style.display = "none";
+    var application = document.getElementById("application");
+    application.style.display = "block";
+  }
+  recaptcha_callback();
+}
+
+function logOutFunction() {
+  let deleteContainer = document.getElementById("deleteContainer");
+  deleteContainer.style.display = "block";
+  let application = document.getElementById("application");
+  application.style.display = "none";
+  window.location.reload();
+}
+
+function rejectCookieFunction() {
+  let rejectPopup = document.getElementById("rejectPopup");
+  rejectPopup.style.display = "block";
+  document.getElementById("cookiePopup").style.display = "none";
+}
+
+// when users click the raccept button
+let acceptNewCookie = document.getElementById("cookieConsent");
+acceptNewCookie.onclick = function () {
+  createNewCookie(cookieName, cookieValue, cookieExpireDays);
+  //uppdaterar
+  window.location.reload();
+};
+
+// function to set cookie in web browser
+let createNewCookie = function (cookieName, cookieValue, cookieExpireDays) {
+  let currentDate = new Date();
+  currentDate.setTime(
+    currentDate.getTime() + cookieExpireDays * 24 * 60 * 60 * 1000
+  );
+  let expires = "expires=" + currentDate.toGMTString();
+  document.cookie = cookieName + "=" + cookieValue + ";" + expires + ";path=/";
+};
